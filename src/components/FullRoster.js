b5ff180090143b1bb77389e581a0f148178d7edf
kumin <273423480@qq.com>
2017-11-14 15:14:05 +0800

    test Immutable.js code

--- src/components/FullRoster.js
+++ src/components/FullRoster.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import Immutable from 'immutable';
 
 class FullRoster extends Component{
     render(){
+        let $map1 = Immutable.Map({a:1, b:3});
+        let $map2 = $map1.set('a', 2);
         return (
-            <h3>this is all rosters</h3>
-        )
+            <div>
+                <h3>this is all rosters</h3>
+                <p>{"$map1:"+$map1.get("a")+"\n$map2:"+$map2.get("a")}</p>
+            </div>
+        );
     }
 }
 
